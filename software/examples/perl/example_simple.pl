c866e9c73dc5abff21d19a4ebc6062214ea1404a
Olaf Lüke <olaf@tinkerforge.com>
2015-05-05 11:35:24 +0200

    Add Perl examples

---
+++ software/examples/perl/example_simple.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletAnalogOutV2;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'XYZ'; # Change to your UID
+
+my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
+my $ao = Tinkerforge::BrickletAnalogOutV2->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Set output voltage to 3.3V
+$ao->set_output_voltage(3300);
+
+print "Press any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
