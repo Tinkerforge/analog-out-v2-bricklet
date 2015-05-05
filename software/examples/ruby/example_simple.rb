4a16ad5013d0c6bd47e0ce509d5db4cc30fcec91
Olaf Lüke <olaf@tinkerforge.com>
2015-05-05 11:38:09 +0200

    Add Ruby example

---
+++ software/examples/ruby/example_simple.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# -*- ruby encoding: utf-8 -*-
+
+require 'tinkerforge/ip_connection'
+require 'tinkerforge/bricklet_analog_out_v2'
+
+include Tinkerforge
+
+HOST = 'localhost'
+PORT = 4223
+UID = 'XYZ' # Change to your UID
+
+ipcon = IPConnection.new # Create IP connection
+ao = BrickletAnalogOutV2.new UID, ipcon # Create device object
+
+ipcon.connect HOST, PORT # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Set output voltage to 3.3V
+ao.set_output_voltage 3300
+
+puts 'Press key to exit'
+$stdin.gets
+ipcon.disconnect
