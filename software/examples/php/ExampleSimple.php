5dd3743b3eb917ac5275405d43bc8a91e42ff547
Olaf Lüke <olaf@tinkerforge.com>
2015-05-05 11:36:46 +0200

    Add PHP example

---
+++ software/examples/php/ExampleSimple.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once('Tinkerforge/IPConnection.php');
+require_once('Tinkerforge/BrickletAnalogOutV2.php');
+
+use Tinkerforge\IPConnection;
+use Tinkerforge\BrickletAnalogOutV2;
+
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = 'XYZ'; // Change to your UID
+
+$ipcon = new IPConnection(); // Create IP connection
+$ao = new BrickletAnalogOutV2(UID, $ipcon); // Create device object
+
+$ipcon->connect(HOST, PORT); // Connect to brickd
+// Don't use device before ipcon is connected
+
+// Set output voltage to 3.3V
+$ao->setOutputVoltage(3300);
+
+echo "Press key to exit\n";
+fgetc(fopen('php://stdin', 'r'));
+$ipcon->disconnect();
+
+?>
